e27d703e7bb88b861baa193e84545680063ce9eb
Muaz Ismail Mohammed <6muazx@gmail.com>
2026-02-01 20:44:47 +0530

    fix: replace deprecated lazyvim.util with LazyVim global

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,7 +10,7 @@ return {
     dependencies = {
       "jose-elias-alvarez/typescript.nvim",
       init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
+        LazyVim.lsp.on_attach(function(_, buffer)
           vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { buffer = buffer, desc = "Rename File" })
         end)
